d637bd3021e3002f5e4dcacafbc8cf1d32c62186
Henrik W. Moe <henrik.walker.moe@bekk.no>
2017-04-14 12:37:55 +0200

    Added high scores

--- src/QuizApp/Quiz.Web/Areas/Scores/Controllers/HighScoresController.cs
+++ src/QuizApp/Quiz.Web/Areas/Scores/Controllers/HighScoresController.cs
@@ -1,12 +1,34 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using Quiz.DataAccess.Quiz;
+using Quiz.DataAccess.Quiz.InMemory;
+using Quiz.Web.Areas.Scores.Models;
+using WebGrease.Css.Extensions;
 
 namespace Quiz.Web.Areas.Scores.Controllers
 {
     public class HighScoresController : Controller
     {
+        private readonly IQuizItemRepository _quizItemRepository = InMemoryQuizItemRepository.Instance;
+        private readonly InMemoryQuizTakingRepository _quizTakingRepository = InMemoryQuizTakingRepository.Instance;
+
         public ActionResult Index()
         {
-            return View();
+            var quizTakings = _quizTakingRepository.GetAll();
+            
+            var model = HighScoreViewModel.MapFromDataModel(quizTakings);
+            model.ForEach(LoadRelationshipProperties);
+            model = model
+                .OrderByDescending(o => o.Score)
+                .ThenBy(o => o.Ended);
+
+            return View("Index", model);
+        }
+
+        private void LoadRelationshipProperties(HighScoreViewModel model)
+        {
+            var quizItem = _quizItemRepository.Get(model.QuizItemId);
+            model.QuizItemName = quizItem.Name;
         }
     }
 }
---
+++ src/QuizApp/Quiz.Web/Areas/Scores/Models/HighScoreViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Quiz.DataAccess.Quiz;
+
+namespace Quiz.Web.Areas.Scores.Models
+{
+    public class HighScoreViewModel
+    {
+        [DisplayName("Quiz Id")]
+        public int QuizItemId { get; set; }
+        
+        [DisplayName("Quiz Name")]
+        public string QuizItemName { get; set; }
+
+        [DisplayName("Score")]
+        public double Score { get; set; }
+
+        [DisplayName("Participant Name")]
+        public string ParticipantName { get; set; }
+
+        [DisplayName("Ended at")]
+        public DateTime? Ended { get; set; }
+
+        public static IEnumerable<HighScoreViewModel> MapFromDataModel(IEnumerable<QuizTaking> quizTakings)
+        {
+            var viewModel = new List<HighScoreViewModel>();
+            foreach (var quizTaking in quizTakings)
+            {
+                viewModel.Add(
+                    new HighScoreViewModel
+                    {
+                        QuizItemId = quizTaking.QuizItemId,
+                        ParticipantName = quizTaking.ParticipantName,
+                        Score = quizTaking.Score,
+                        Ended = quizTaking.Ended
+                    });
+            }
+            return viewModel;
+        }
+    }
+}
